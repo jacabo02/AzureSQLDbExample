128d066cd78d772a3f2322fcb91c59b0ced268af
hdurmus <hasan.durmus@tacirler.com.tr>
2021-09-16 15:46:25 +0300

    Add project files.

---
+++ CORSExampleAPI/Controllers/CourseController.cs
@@ -0,0 +1,27 @@
+﻿using CORSExampleAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CORSExampleAPI.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly CourseService _courseService;
+        private readonly IConfiguration _configuration;
+        public CourseController(CourseService courseService, IConfiguration configuration)
+        {
+            _courseService = courseService;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(_courseService.GetCourses(_configuration.GetConnectionString("SQLConnection")));
+        }
+    }
+}
---
+++ CORSExampleAPI/Services/CourseService.cs
@@ -0,0 +1,54 @@
+﻿using CORSExampleAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CORSExampleAPI.Services
+{
+    public class CourseService
+    {
+
+        public List<Course> CourseList;
+
+        private SqlConnection GetConnection(string connectionstring)
+        {
+            return new SqlConnection(connectionstring);
+        }
+
+        public CourseService()
+        {
+        }
+
+        public IEnumerable<Course> GetCourses(string connectionString)
+        {
+            CourseList = new List<Course>();
+            string query = "Select CourseID, CourseName, Rating From Course";
+
+            using(SqlConnection connection = GetConnection(connectionString))
+            {
+                connection.Open();
+                using(SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using(SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while(reader.Read())
+                        {
+                            CourseList.Add(
+                                new Course
+                                {
+                                    CourseID = Convert.ToInt32(reader["CourseID"]),
+                                    CourseName = Convert.ToString(reader["CourseName"]),
+                                    Rating = Convert.ToDecimal(reader["Rating"])
+                                });
+                        }
+                    }
+                }
+                connection.Close();
+            }
+
+            return CourseList;
+        }
+    }
+}
